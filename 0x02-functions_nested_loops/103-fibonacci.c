c2b81154f39985b49e3eb949f3705a3654108553
Abduleshekure <abduleshekurehassen03@gmail.com>
2022-10-18 19:48:17 -0700

    fix bug

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -18,10 +18,11 @@ int main(void)
 			break;
 
 		if ((fibsum % 2) == 0)
+		{
 			tot_sum += fibsum;
 
-		fib1 = fib2;
-		fib2 = fibsum;
+			fib1 = fib2;
+			fib2 = fibsum;
 		}
 		printf("%.0f\n", tot_sum);
 
